d567f699cf77f276087fc80ae23f803bf8989de4
Kovács Dorina <kovacsdorina2@gmail.com>
2022-11-24 17:34:31 +0100

    Modal működik

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,18 @@
 // * Github: https://github.com/Kdorina
 // * Licenc: GNU GPL
 // */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ 
+
+  constructor() {}
+
   title = 'Telek területszámítás';
   aSide!: number;
   bSide!: number;
@@ -22,13 +26,46 @@ export class AppComponent {
   halfarea!: number;
   showHalfaera!: any;
   showAera!: any;
+  about!: any;
+
+  //IMAGE
+  path: string = '../assets/images/telek.png';
+  alttext: string = 'A kép leírása';
+
+//MODAL
+  showModal : any;
+
+  clickModal(){
+   this.showModal = true;
+  }
+
+  closeBtn(){
+  this.showModal = false;
+  }
 
  calc():void{
   this.halfarea = this.aSide*this.bSide;
   this.area = this.halfarea*2;
   this.showHalfaera = true;
   this.showAera = true;
- }
+  this.about = true;
+ };
 
+ ngOnInit(): void {
+    // this.formModal = new window.bootstrap.Modal(
+    //   document.getElementById('myModal')
+    
+  }
+  
 
+//  openModal(){
+//     // // this.formModal.show();
+//     // this.formModal = true;
+//     this.modal = true;
+   
+//   }
+//   close(){
+//     // this.formModal.hide();
+//     this.modal = true;
+//   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
